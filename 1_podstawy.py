f4ed348992b31b3d7467b6d84a655d8885b76815
skowalak <basteq7@gmail.com>
2020-03-30 08:41:18 +0200

    commit

---
+++ 1_podstawy.py
@@ -0,0 +1,28 @@
+
+'''
+#potęgowanie
+pow=2**10
+print(pow)
+
+text='CDV' #zadeklarowany string
+print(text*2)
+
+
+#pobieranie danych z klawiatury
+
+name=input("Podaj imię:")
+print("Twoje imię to: " + name)
+
+surname=input("Podaj nazwisko:")
+print("Twoje imie to: " + name + ", a nazwisko to: " + surname)
+
+
+lenghtName=str(len(name))
+print("Dlugosc imienia to:" + lenghtName)
+
+lenghtSurname=str(len(name))
+print("Dlugosc nazwiska to:" + lenghtSurname)
+print(type(lenghtName))
+print(type(lenghtSurname))
+
+'''
---
+++ 3_imie_nazwisko.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+name=input("Podaj imie:")
+surname=input("Podaj nazwisko:")
+yearsOld=input("Podaj ile masz lat:")
+print("Twoje imie i nazwisko: " + name  + " " + surname + " , a Twoj wiek to: " + yearsOld + " lat ")
+
+
+print("\nPodaj narodowość:", end="")
+nationality=input()
+surname="Kowalski"
+firstLetter=surname[0]
+print (firstLetter)
+
+lastLetter=surname[len(surname) -1]
+print(lastLetter)
+
+#konwersja danych
+
+a="10"
+print(type(a))
+a=int(a)
+print(type(a))
+
+b=6
+print(type(b)) #int
+b/=2 #b=b/2
+print(type(b)) #float
+print(b)
+
+print()
+surname="Nowakowski"
+print(surname[0]) # N
+print(surname[0:2]) # No
+print(surname[-2]) # k
+print(surname[-2:]) # ki
+print(surname[:-2]) # Nowakows
+print(surname[:-2:2]) # Nwkw
+
+
+
